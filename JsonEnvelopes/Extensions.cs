c00b7f3d2c372ae1b3fea6d865281078d9f8e0a9
Dane Vinson <dane.vinson@sprbrk.com>
2020-11-10 20:01:15 -0800

    Signed JsonEnvelopes and re-versioned

--- JsonEnvelopes/Extensions.cs
+++ JsonEnvelopes/Extensions.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
 using System.Text.Json;
+
+[assembly: AssemblyKeyFile("JsonEnvelopes.snk")]
 
 namespace JsonEnvelopes
 {
